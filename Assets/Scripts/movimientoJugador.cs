7e8928112bb44bf65aa6ac761153765360329e0a
arco03 <dmglux13@gmail.com>
2024-02-08 18:08:35 -0500

    movimiento y personaje
    se agrego el sprite del personaje principal y su respectivo movimiento.

---
+++ Assets/Scripts/movimientoJugador.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimientoJugador : MonoBehaviour
+{
+    public float speed;
+    public int x;
+    public int y;
+    public bool canMove;
+
+    private void Awake()
+    {
+        canMove = true;
+    }
+
+    private void Update()
+    {
+        int moveX = (int)Input.GetAxisRaw("Horizontal");
+        if (moveX != 0 && canMove)
+        {
+            x += moveX;
+            canMove = false;
+        }
+
+        Vector2 currentPosition = new Vector2(
+            transform.position.x,
+            transform.position.y
+            );
+
+        if (currentPosition == CalcularDireccion(x, y))
+        {
+            canMove = true;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 finalPosition = CalcularDireccion(x, y);
+        transform.position = Vector2.MoveTowards(transform.position, finalPosition, speed);
+    }
+
+    Vector2 CalcularDireccion(int x, int y)
+    {
+        return new Vector2(x + 0.5f, y + 0f);
+    }
+}
